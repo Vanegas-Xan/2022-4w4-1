b60e72d6d1ca1e74558563f9a46d5cc1027fee27
Vanegas-Xan <e1872649@cmaisonneuve.qc.ca>
2022-02-11 19:16:35 -0500

    Premier commit: Menu svg

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php
+function cidw_4w4_enqueue() {
+// wp_enqueue_style('style_css', get_template_directory_uri() . '/style.css')
+    wp_enqueue_style('style_css', get_template_directory_uri() . '/style.css', 
+    array(), 
+    filemtime(get_template_directory() . '/style.css'),
+     false);
+
+     wp_enqueue_style('cidw_4w4_police_google','https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap', false);
+} 
+
+add_action("wp_enqueue_scripts", "cidw_4w4_enqueue");
+
+//-------------------------------------------------------------Enregistrer le menu
+
+function cidw_4w4_enregistre_mon_menu() {
+  register_nav_menu( 'principal', __( 'Menu principal', 'cidw_4w4' ) );
+}
+
+add_action( 'after_setup_theme', 'cidw_4w4_enregistre_mon_menu' );
+
+//-------------------------------------------------------------Filtre chacun des choixs du menu
+
+function cidw_4w4_filtre_le_menu($mon_objet){
+
+foreach($mon_objet as $cle => $valeur){
+
+//$valeur->title = substr($valeur->titre, 0, 7);
+
+$valeur->title = wp_trim_words($valeur->titre, 4, "...");
+}
+
+return $mon_objet;
+}
+
+add_filter("wp_nav_menu_objects", "cidw_4w4_filtre_le_menu");
+
+?>
---
+++ header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class("site"); ?> >
+<section class="site__header__titre">
+    <h1 class="header__principal"><?php echo get_bloginfo( 'name'); ?></h1>
+    <h2 class="header__secondaire"><?php echo get_bloginfo( 'description'); ?></h2>
+</section >
+<section class="site__header__utile">
+<div class="icone">
+<a class="icone__lien" href="#"><svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#90d5c7"><path d="M3 12v3c0 1.657 3.134 3 7 3s7-1.343 7-3v-3c0 1.657-3.134 3-7 3s-7-1.343-7-3z"></path><path d="M3 7v3c0 1.657 3.134 3 7 3s7-1.343 7-3V7c0 1.657-3.134 3-7 3S3 8.657 3 7z"></path><path d="M17 5c0 1.657-3.134 3-7 3S3 6.657 3 5s3.134-3 7-3 7 1.343 7 3z"></path></svg>
+</a>
+
+<a class="icone__lien" href="#">
+    <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#90d5c7"><path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zm4.211-10.724a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z"></path></svg>
+</a>
+<a class="icone__lien" href="#">
+    <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#90d5c7"><path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zm4.211-10.724a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z"></path></svg>
+</a>
+<a class="icone__lien" href="#">
+    <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#90d5c7"><path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zm4.211-10.724a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z"></path></svg>
+</a>
+<a class="icone__lien" href="#">
+    <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#90d5c7"><path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zm4.211-10.724a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z"></path></svg>
+</a>
+</div>
+
+<form action="" class="recherche"></form>
+<input type="text" class="recherche_input">
+<button class="recherche__bouton"></button>
+</section>
+
+<header class="site__header">
+    
+</header>
+<section class="site__barre">
+    <input type="checkbox"  id="chk-burger">
+    <label for="chk-burger" id="burger">
+  
+    <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#ccc">
+    <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    </label>
+<?php wp_nav_menu(array("menu" => "principal",
+                                        "container" => "nav")); ?>
+ 
+</section>
+    
