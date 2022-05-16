f09c4bd60a0f7281939e24091541b9ca59ff0145
Vanegas-Xan <e1872649@cmaisonneuve.qc.ca>
2022-05-15 21:01:03 -0400

    Avancement TP2 : ajout d'un flex sur la section des widjets

--- front-page.php
+++ front-page.php
@@ -12,11 +12,11 @@
 
     <h1 class="galerie"> Galerie Gutenberg</h1>
 
-    <?php if (have_posts()): while(have_posts()):the_post() ?>
+    <?php if (have_posts()) : the_post(); ?>
             
     <?php the_title(); ?>
     <?php the_content(); ?>
-             <?php endwhile ?>    
+            
               
     <?php endif ?>
      
